bfc7bff99a9f4c34af3fdda1bab8c3eba84b6eba
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-02-10 07:26:24 +0500

    Add embed chatbot page with copy button and preview

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -2,7 +2,7 @@ import { scalekit } from "@/lib/scalekit";
 import { NextRequest, NextResponse } from "next/server";
 
 export async  function GET(req:NextRequest){
-    const redirectUri=`${process.env.Next_Public_APP_URL}/api/auth/callback`
+    const redirectUri=`${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback`
    const url= scalekit.getAuthorizationUrl(redirectUri)
    console.log(url);
 
--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -4,6 +4,6 @@ import { NextRequest, NextResponse } from "next/server";
 export async  function GET(req:NextRequest){
   const cookiesStore = await cookies()
   cookiesStore.delete("access_token")
-  return NextResponse.redirect(`${process.env.Next_Public_APP_URL}`)
+  return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}`)
    
 }
